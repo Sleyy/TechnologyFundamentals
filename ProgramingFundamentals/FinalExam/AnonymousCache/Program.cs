dc2b6705400a789cb91750360e6b0face0a6078f
Sleya <yehiazz98246@abv.bg>
2017-11-06 16:58:37 +0200

    Aced Exam - or sort of...

---
+++ ProgramingFundamentals/FinalExam/AnonymousCache/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousCache
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, int>> data = new Dictionary<string, Dictionary<string, int>>();
+            Dictionary<string, Dictionary<string, int>> cache = new Dictionary<string, Dictionary<string, int>>();
+
+            string[] input = Console.ReadLine()
+                .Split(new string[] { " ", "-", ">", "|", "\t" }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            while (input[0] != "thetinggoesskrra")
+            {
+                string dataSet = string.Empty;
+                string dataKey = string.Empty;
+                int dataSize = 0;
+                if (input.Length == 1)
+                {
+                    dataSet = input[0];
+                    data.Add(dataSet, new Dictionary<string, int>());
+                    if (cache.ContainsKey(dataSet))
+                    {
+                        foreach (var box in cache)
+                        {
+                            foreach (var f in box.Value)
+                            {
+                                data[dataSet].Add(f.Key,f.Value);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    dataKey = input[0];
+                    dataSize = int.Parse(input[1]);
+                    dataSet = input[2];
+                    if (data.ContainsKey(dataSet))
+                    {
+                        data[dataSet].Add(dataKey, dataSize);
+                    }
+                    else
+                    {
+                        if (!cache.ContainsKey(dataSet))
+                        {
+                            cache.Add(dataSet, new Dictionary<string, int>());
+                            cache[dataSet].Add(dataKey, dataSize);
+                        }
+                        else
+                        {
+                            cache[dataSet].Add(dataKey,dataSize);
+                        }
+                        
+                    }
+
+
+                }
+
+                input = Console.ReadLine()
+                    .Split(new string[] { " ", "-", ">", "|","\t" }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+            }
+            BigInteger sum = 0;
+            BigInteger totalSum = 0;
+            var firstKey = string.Empty;
+            if (data.Count != 0)
+            {
+               
+                
+                foreach (var box in data)
+                {
+                    sum = 0;
+                    foreach (var b in box.Value)
+                    {
+                       
+                            sum += b.Value;
+                       
+                    }
+                    if (sum > totalSum)
+                    {
+                        totalSum = sum;
+                         firstKey = box.Key;
+
+                    }
+                }
+                
+                foreach (var d in data)
+                {
+                    if (d.Key == firstKey)
+                    {
+                        Console.WriteLine($"Data Set: {d.Key}, Total Size: {totalSum}");
+                        foreach (var f in d.Value)
+                        {
+                            Console.WriteLine($"$.{f.Key}");
+                        }
+                    }
+
+                }
+            }
+            
+        }
+    }
+}
---
+++ ProgramingFundamentals/FinalExam/AnonymousDownsite/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace AnonymousDownsite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfWebsitesDown = int.Parse(Console.ReadLine());
+            int securityKey = int.Parse(Console.ReadLine());
+            decimal totalLoss = 0;
+            List<string> websites = new List<string>();
+            BigInteger securityToken = securityKey;
+            for (int i = 0; i < numberOfWebsitesDown-1; i++)
+            {
+                securityToken *= securityKey;
+            }
+            for (int i = 0; i < numberOfWebsitesDown; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(new string[] {" ", "\t"}, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string siteName = input[0];
+                decimal siteVisits = decimal.Parse(input[1]);
+                decimal siteCommercialPricePerVisit = decimal.Parse(input[2]);
+                websites.Add(siteName);
+                 totalLoss += siteVisits * siteCommercialPricePerVisit;
+            }
+            Console.WriteLine(string.Join("\r\n",websites));
+            Console.WriteLine($"Total Loss: {totalLoss:f20}");
+            Console.WriteLine($"Security Token: {securityToken}");
+        }
+    }
+}
